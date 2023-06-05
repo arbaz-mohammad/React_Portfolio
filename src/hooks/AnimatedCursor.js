917bbbf9fd4ef0f89764af26fb0df5b580449955
ARBAZ MOHAMMAD <arbazmohammad117@gmail.com>
2023-06-05 13:54:37 +0530

    first commmmit

---
+++ src/hooks/AnimatedCursor.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AnimatedCursor() {
+  return <div style={{ cursor: 'auto' }} />;
+}
+
+export default AnimatedCursor;
---
+++ src/pages/lucky/lucky.js
@@ -0,0 +1,11 @@
+// Assuming you have an HTML button with the id "myButton"
+const button = document.getElementById("button_p");
+
+// Function to handle button click event
+function handleClick() {
+  // Replace "https://example.com" with the actual website URL you want to link to
+  window.location.href = "https://lucky-manatee-2b2d39.netlify.app/";
+}
+
+// Attach the event listener to the button
+button.addEventListener("click", handleClick);
